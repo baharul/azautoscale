e01677e1b8217939b83067f2dcdab38e204ecd65
baharul islam <bahkhan@mafcarrefour.com>
2021-07-25 17:39:35 +0400

    only code change with genesis-core(terraformrun - check)

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
-//sample cde
+//sample cder
 
 @SpringBootApplication
 public class Application {
