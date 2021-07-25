e9bc4239b4abb521a17e1431497da5ca2d104fbc
baharul islam <bahkhan@mafcarrefour.com>
2021-07-25 17:02:21 +0400

    only code change with genesis core repo change to check terraform run

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
-//sample
+//sample cde
 
 @SpringBootApplication
 public class Application {
